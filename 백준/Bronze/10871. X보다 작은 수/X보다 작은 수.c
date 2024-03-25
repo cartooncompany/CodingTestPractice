36c42bab10db9b38208b9de0775c3e575f9cbd27
박태준 <162271904+cartooncompany@users.noreply.github.com>
2024-03-25 22:34:25 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, n, x;
+    scanf("%d %d", &n, &x);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a);
+        if (a < x) printf("%d ", a);
+    }   
+    
+    return 0;
+}
