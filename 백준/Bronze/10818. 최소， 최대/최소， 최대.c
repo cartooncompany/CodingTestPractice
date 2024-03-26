b3e9c0f4b1764861105c7f5a6de7984f993ac58c
박태준 <162271904+cartooncompany@users.noreply.github.com>
2024-03-26 20:13:28 +0900

    [Bronze III] Title: 최소, 최대, Time: 176 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, num, min, max;
+    scanf("%d", &n);
+    scanf("%d", &num);
+    min = max = num;
+
+    for (int i = 1; i < n; i++) {
+        scanf("%d", &num);
+        if (num < min) min = num;
+        if (num > max) max = num;
+    }
+
+    printf("%d %d", min, max);
+
+    return 0;
+}
