7fcb281eef99817638a3d10fff665bdecdca504b
박태준 <162271904+cartooncompany@users.noreply.github.com>
2024-04-23 17:05:12 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/0/120820. 나이 출력/나이 출력.c
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#define YEAR 2022
 
 int solution(int age) {
-    return (YEAR - age)+1;
+    int answer = 0;
+    return answer = 2022 - age + 1;
 }
