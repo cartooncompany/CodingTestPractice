30bc7460f440aab6228a314222dabf2da86d06fc
박태준 <162271904+cartooncompany@users.noreply.github.com>
2024-04-23 16:07:43 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.01 ms, Memory: 4.54 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.c
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.c
@@ -4,11 +4,11 @@
 
 int solution(int n) {
     int answer = 0;
-    for(int i =1; i <= n; i++) {
-        if(n%i==1){
-            answer = i;
+    for(int i = 1; i <= n; i++){
+        if(n % i == 1){ 
+        answer = i;
             break;
         }
-    }   
+    }
     return answer;
 }
