8ca7ef3b4627071933045ab00f4cb4dfcf521692
박태준 <162271904+cartooncompany@users.noreply.github.com>
2024-04-23 16:02:34 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.c
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.c
@@ -5,9 +5,8 @@
 // arr_len은 배열 arr의 길이입니다.
 double solution(int arr[], size_t arr_len) {
     double answer = 0;
-    double n = arr[0];
-    for(int i = 1; i < arr_len; i++){
-        n += arr[i];
+    for(int i = 0; i < arr_len; i++){
+        answer += arr[i];
     }
-    return answer = n/arr_len;
+    return answer/arr_len;
 }
