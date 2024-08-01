7de3a0f3c1c30643d213591314dd27d0e629771b
박태준 <162271904+cartooncompany@users.noreply.github.com>
2024-08-01 17:11:48 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    
+    char n;
+    cin>>n;
+    
+    if(n == 77){
+        cout<<"MatKor"<<"\n";
+    } else if(n == 87){
+        cout<<"WiCys"<<"\n";
+    } else if(n == 67){
+        cout<<"CyKor"<<"\n";
+    } else if(n == 65){
+        cout<<"AlKor"<<"\n";
+    } else if(n == 36){
+        cout<<"$clear"<<"\n";
+    }
+    
+    return 0;
+}
