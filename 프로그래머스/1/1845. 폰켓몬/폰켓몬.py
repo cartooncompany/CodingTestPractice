cdc06f5d31ffec560018af0c0b5d1552e19ec5d7
박태준 <162271904+cartooncompany@users.noreply.github.com>
2024-09-09 19:57:14 +0900

    [level 1] Title: 폰켓몬, Time: 1.88 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,6 +1,10 @@
 def solution(nums):
-    unique_pokemons = set(nums)
+    hash_set = set()
     
+    for pokemon in nums:
+        hash_set.add(hash(pokemon))
+    
+    unique_count = len(hash_set)
     max_selectable = len(nums) // 2
     
-    return min(len(unique_pokemons), max_selectable)
+    return min(unique_count, max_selectable)
