58b9782aded5001d078f3ef13736ae4efb24e59e
박태준 <162271904+cartooncompany@users.noreply.github.com>
2024-09-09 20:22:03 +0900

    [level 1] Title: 폰켓몬, Time: 0.98 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,10 +1,8 @@
 def solution(nums):
-    hash_poke = set()
-    
+    hash = dict()
     for i in nums:
-        hash_poke.add(hash(i))
-        
-    pokeList = len(hash_poke)
-    listLength = len(nums) // 2
-    
-    return min(pokeList, listLength)
+        hash[i] = 1
+    if len(hash) >= (len(nums)/2):
+        return len(nums)/2
+    else:
+        return len(hash)
