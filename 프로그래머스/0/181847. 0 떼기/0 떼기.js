9ae54ba36f5c52ba7a76b220d2bec7af9d3e6704
박태준 <162271904+cartooncompany@users.noreply.github.com>
2024-10-23 15:38:20 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -0,0 +1,12 @@
+function solution(n_str) {
+    let result = "";
+    for(let i = 0; i < n_str.length; i++){
+        if(n_str[i] == '0'){
+            continue;
+        } else {
+            result = n_str.slice(i)
+            break;
+        }
+    }
+    return result;
+}
