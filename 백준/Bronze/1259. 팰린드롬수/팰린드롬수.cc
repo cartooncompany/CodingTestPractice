604092567a33430b129d36142dde1f1ab466622a
박태준 <162271904+cartooncompany@users.noreply.github.com>
2024-12-29 21:12:25 +0900

    [Bronze I] Title: 팰린드롬수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
@@ -11,7 +11,7 @@ int main(){
         cin >> num;
         if(num == "0") break;
         
-        for(int i = 0; i <= num.length()/2; i++){
+        for(int i = 0; i < num.length()/2; i++){
             if(num[i] != num[num.length()-1-i]){
                 pal = 0;
                 break;
